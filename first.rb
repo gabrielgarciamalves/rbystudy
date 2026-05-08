046ff1c42a2ffa99d6bc1f939117e1e97fe6bae5
gabriel garcia <gabriel.garcia@embras.net>
2026-05-07 22:47:26 -0300

    some variables and printin' some data

---
+++ first.rb
@@ -0,0 +1,9 @@
+# create an array
+address = [1,2,3,4,5,6,7,8,9]
+name = 'gabriel' # create a string
+age = 25 # create an integer
+
+p address # showing the array
+p address[0] # showing the first element of the array
+p name # showing the string
+p age # showing the integer
