d0bcbe3e786b882a527b856712c8b26e19dca9b0
Gabriel Garcia <gabriel.garcia@embras.net>
2026-05-12 23:13:23 -0300

    ArgumentsChomp

---
+++ MainArguments.rb
@@ -0,0 +1,16 @@
+# Example 1: Basic input
+puts "Enter your name:"
+name = gets.chomp
+puts "Hello, #{name}!"
+
+# Example 2: Input with conversion
+puts "Enter your age:"
+age = gets.chomp.to_i
+puts "You are #{age} years old."
+
+# Example 3: Multiple inputs
+puts "Enter your first name:"
+first_name = gets.chomp
+puts "Enter your last name:"
+last_name = gets.chomp
+puts "Full name: #{first_name} #{last_name}"
