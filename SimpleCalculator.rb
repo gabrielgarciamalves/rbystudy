581842201c485f1cfed9a7dc5121e4ebdc2227b7
gabriel garcia <gabriel.garcia@embras.net>
2026-05-14 21:36:53 -0300

    add sub and divide calculation

--- SimpleCalculator.rb
+++ SimpleCalculator.rb
@@ -7,3 +7,5 @@ puts "Enter another number:"
 num2 = gets.chomp
 
 puts "First number multiplied by the second number is: #{num1.to_f * num2.to_f}"
+puts "First number divide by the second number is: #{num1.to_f / num2.to_f}"
+puts "First number sub by the second number is: #{num1.to_f - num2.to_f}"
