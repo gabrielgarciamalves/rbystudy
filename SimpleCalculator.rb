746880845f775efac683fd7c044dc9d2d0a34442
gabriel garcia <gabriel.garcia@embras.net>
2026-05-14 21:32:43 -0300

    fix

--- SimpleCalculator.rb
+++ SimpleCalculator.rb
@@ -1,9 +1,9 @@
 puts "Simple Calculator"
-20.times { print "-" }
+20.times { print "-\n" }
 puts "Enter a number:"
-num1 = gets.chomp
+num1 = gets.chomp.to_f
 puts "Enter another number:"
-num2 = gets.chomp
+num2 = gets.chomp.to_f
 
 puts "First number multiplied by the second number is:
 #{num1 * num2}."
